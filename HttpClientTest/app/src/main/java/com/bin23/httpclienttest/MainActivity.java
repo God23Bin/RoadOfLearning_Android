6f799cdfcdad9bf99fff022519396b1dd04541ca
god23bin <479017580@qq.com>
2021-04-16 09:44:17 +0800

    HttpClient的使用

--- HttpClientTest/app/src/main/java/com/bin23/httpclienttest/MainActivity.java
+++ HttpClientTest/app/src/main/java/com/bin23/httpclienttest/MainActivity.java
@@ -2,13 +2,83 @@ package com.bin23.httpclienttest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
 
+
+
+public class MainActivity extends Activity implements View.OnClickListener {
+
+    private Button btnGet;
+    private WebView wView;
+    public static final int SHOW_DATA = 0X123;
+    private String detail = "";
+
+    @SuppressLint("HandlerLeak")
+    private Handler handler = new Handler() {
+        public void handleMessage(Message msg) {
+            if(msg.what == SHOW_DATA)
+            {
+                wView.loadDataWithBaseURL("",detail, "text/html","UTF-8","");
+            }
+        };
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+        setView();
+    }
+
+    private void initView() {
+        btnGet = (Button) findViewById(R.id.btnGet);
+        wView = (WebView) findViewById(R.id.wView);
+    }
+
+    private void setView() {
+        btnGet.setOnClickListener(this);
+        wView.getSettings().setDomStorageEnabled(true);
+    }
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.btnGet) {
+            GetByHttpClient();
+        }
+    }
+    private void GetByHttpClient() {
+        new Thread()
+        {
+            public void run()
+            {
+                try {
+                    HttpClient httpClient = new DefaultHttpClient();
+                    HttpGet httpGet = new HttpGet("http://www.baidu.com");
+                    HttpResponse httpResponse = httpClient.execute(httpGet);
+                    if (httpResponse.getStatusLine().getStatusCode() == 200) {
+                        HttpEntity entity = httpResponse.getEntity();
+                        detail = EntityUtils.toString(entity, "utf-8");
+                        handler.sendEmptyMessage(SHOW_DATA);
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            };
+        }.start();
     }
 }
