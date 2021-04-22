34a94687f3babf40bfdd86da4b936a0ce1bb18ba
god23bin <479017580@qq.com>
2021-04-22 09:09:15 +0800

    TimeTask-定时任务测试

---
+++ TimeTaskTest/app/src/main/java/com/bin23/timetasktest/LongRunningService.java
@@ -0,0 +1,16 @@
+package com.bin23.timetasktest;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class LongRunningService extends Service {
+    public LongRunningService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
