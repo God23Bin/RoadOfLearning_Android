6f47fc871c58b1432092e4dee14a2b1d82d4499e
god23bin <479017580@qq.com>
2021-04-24 22:47:36 +0800

    Gson的使用

--- GsonTest/app/src/main/java/com/bin23/gsontest/MainActivity.java
+++ GsonTest/app/src/main/java/com/bin23/gsontest/MainActivity.java
@@ -3,6 +3,12 @@ package com.bin23.gsontest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.bin23.gsontest.entity.Person;
+import com.google.gson.Gson;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn = (Button) findViewById(R.id.get_json);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String jsonContent = "{\"name\": \"Tom\", \"age\": 20}";
+                Person person = new Gson().fromJson(jsonContent, Person.class);
+
+                Log.d("MainActivity", "Person的name为" + person.getName());
+                Log.d("MainActivity", "Person的age为" + person.getAge());
+            }
+        });
+
+
     }
 }
