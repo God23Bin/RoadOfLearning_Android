57fef1fd89e9e7d9e974cc09923384bbd2d790bf
god23bin <479017580@qq.com>
2021-05-28 14:53:00 +0800

    1.登录逻辑 2.注册跳转

--- MusicDemo/app/src/main/java/com/bin23/music/activities/LoginActivity.java
+++ MusicDemo/app/src/main/java/com/bin23/music/activities/LoginActivity.java
@@ -2,12 +2,17 @@ package com.bin23.music.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
 import com.bin23.music.R;
+import com.bin23.music.utils.UserUtils;
+import com.bin23.music.views.InputView;
 
 public class LoginActivity extends BaseActivity {
+
+    private InputView mIvPhone, mIvPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,20 +27,31 @@ public class LoginActivity extends BaseActivity {
      */
     private void initView() {
         initToolBar(false, "欢迎登录~");
+
+        mIvPhone = fd(R.id.input_phone);
+        mIvPassword = fd(R.id.input_password);
     }
 
     /**
      * 登录
      */
     public void onCommitClick(View view) {
-
+        String phone = mIvPhone.getInputStr();
+        String password = mIvPassword.getInputStr();
+        if (!UserUtils.validateLogin(this, phone, password)) {
+            return;
+        }
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 
     /**
      * 注册
      */
     public void onRegisterClick(View view) {
-
+        Intent intent = new Intent(this, RegisterActivity.class);
+        startActivity(intent);
     }
 
     /**
