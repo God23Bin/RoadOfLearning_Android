80a505e0eb98d5e935af9fb09c605813c8fb06c3
god23bin <479017580@qq.com>
2021-05-28 14:55:35 +0800

    初始化 AndroidUtilCode

--- MusicDemo/app/src/main/java/com/bin23/music/MyApplication.java
+++ MusicDemo/app/src/main/java/com/bin23/music/MyApplication.java
@@ -2,9 +2,14 @@ package com.bin23.music;
 
 import android.app.Application;
 
+import com.blankj.utilcode.util.Utils;
+
 public class MyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        // 初始化 AndroidUtilCode
+        Utils.init(this);
     }
 }
