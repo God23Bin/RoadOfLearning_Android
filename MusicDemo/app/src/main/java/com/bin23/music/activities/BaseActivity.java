b1c284ce1dc0ed7a6a0bb40a7e6709613a85b425
god23bin <479017580@qq.com>
2021-05-30 09:38:17 +0800

    解决后退的动画错误

--- MusicDemo/app/src/main/java/com/bin23/music/activities/BaseActivity.java
+++ MusicDemo/app/src/main/java/com/bin23/music/activities/BaseActivity.java
@@ -38,6 +38,9 @@ public class BaseActivity extends Activity {
             @Override
             public void onClick(View v) {
                 onBackPressed();
+                // 解决动画错误
+                ((Activity)BaseActivity.this).overridePendingTransition(R.anim.open_enter, R.anim.open_exit);
+                ((Activity)BaseActivity.this).overridePendingTransition(R.anim.close_enter, R.anim.close_exit);
             }
         });
     }
