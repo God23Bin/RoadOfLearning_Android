5b0eddad5839936cb8eaaf8cc533f5d4e91c9e82
god23bin <479017580@qq.com>
2021-06-02 12:22:03 +0800

    修改密码，类似Controller层，调用Service层进行修改密码

--- MusicDemo/app/src/main/java/com/bin23/music/activities/UpdatePwActivity.java
+++ MusicDemo/app/src/main/java/com/bin23/music/activities/UpdatePwActivity.java
@@ -6,8 +6,12 @@ import android.os.Bundle;
 import android.view.View;
 
 import com.bin23.music.R;
+import com.bin23.music.utils.UserUtils;
+import com.bin23.music.views.InputView;
 
 public class UpdatePwActivity extends BaseActivity {
+
+    private InputView oldPwd, newPwd, newPwdConfirm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,9 +23,21 @@ public class UpdatePwActivity extends BaseActivity {
 
     private void initView() {
         initToolBar(true, "修改密码");
+        oldPwd = fd(R.id.input_old_password);
+        newPwd = fd(R.id.input_new_password);
+        newPwdConfirm = fd(R.id.input_new_confirm_password);
     }
 
     public void onUpdatePwConfirmBtnClick(View view) {
+        String oldPassword = oldPwd.getInputStr();
+        String password = newPwd.getInputStr();
+        String passwordConfirm = newPwdConfirm.getInputStr();
 
+        boolean result = UserUtils.updatePassword(this, oldPassword, password, passwordConfirm);
+        if(!result){
+            return ;
+        }
+        showToast(this, "成功修改密码~");
+        onBackPressed();
     }
 }
