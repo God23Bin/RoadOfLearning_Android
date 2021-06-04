9db7624367d3cbfed0011fb01db7da5557f27e88
god23bin <479017580@qq.com>
2021-06-04 20:08:58 +0800

    数据迁移、保存音乐数据、删除音乐数据、返回音乐源数据、返回歌单、返回音乐

--- MusicDemo/app/src/main/java/com/bin23/music/helps/RealmHelper.java
+++ MusicDemo/app/src/main/java/com/bin23/music/helps/RealmHelper.java
@@ -2,10 +2,10 @@ package com.bin23.music.helps;
 
 import android.content.Context;
 
-//import com.bin23.music.migration.Migration;
-//import com.bin23.music.model.AlbumModel;
-//import com.bin23.music.model.MusicModel;
-//import com.bin23.music.model.MusicSourceModel;
+import com.bin23.music.migration.Migration;
+import com.bin23.music.model.AlbumModel;
+import com.bin23.music.model.MusicModel;
+import com.bin23.music.model.MusicSourceModel;
 import com.bin23.music.model.UserModel;
 import com.bin23.music.utils.DataUtil;
 
@@ -26,34 +26,34 @@ public class RealmHelper {
         mRealm = Realm.getDefaultInstance();
     }
 
-//    /**
-//     * Realm数据库发生结构性变化（模型或者模型中的字段发生了新增、修改、删除）的时候，我们就需要对数据库进行迁移
-//     *
-//     * @return
-//     */
-//    // 返回RealmConfiguration
-//    private static RealmConfiguration getRealmConf() {
-//        return new RealmConfiguration.Builder()
-//                .schemaVersion(1)
-//                .migration(new Migration())
-//                .build();
-//    }
+   /**
+    * Realm数据库发生结构性变化（模型或者模型中的字段发生了新增、修改、删除）的时候，我们就需要对数据库进行迁移
+    *
+    * @return
+    */
+   // 返回RealmConfiguration
+   private static RealmConfiguration getRealmConf() {
+       return new RealmConfiguration.Builder()
+               .schemaVersion(2)
+               .migration(new Migration())
+               .build();
+   }
 
-//    /**
-//     * 告诉Reaml数据需要迁移，并设置新版本号
-//     */
-//    public static void migration() {
-//        RealmConfiguration conf = getRealmConf();
-//        // Realm 设置最新的配置
-//        Realm.setDefaultConfiguration(conf);
-//        // 告诉Realm数据需要迁移
-//        try {
-//            Realm.migrateRealm(conf);
-//        } catch (FileNotFoundException e) {
-//            e.printStackTrace();
-//        }
-//
-//    }
+   /**
+    * 告诉Reaml数据需要迁移，并设置新版本号
+    */
+   public static void migration() {
+       RealmConfiguration conf = getRealmConf();
+       // Realm 设置最新的配置
+       Realm.setDefaultConfiguration(conf);
+       // 告诉Realm数据需要迁移
+       try {
+           Realm.migrateRealm(conf);
+       } catch (FileNotFoundException e) {
+           e.printStackTrace();
+       }
+
+   }
 
     /**
      * 关闭数据库
@@ -74,6 +74,9 @@ public class RealmHelper {
         mRealm.commitTransaction();
     }
 
+    /**
+     * 返回所有用户
+     */
     public List<UserModel> getAllUser() {
         RealmQuery<UserModel> query = mRealm.where(UserModel.class);
         return query.findAll();
@@ -116,58 +119,59 @@ public class RealmHelper {
     }
 
 //    /**
-//     * 用户登录，存放数据
+//     * 用户登录，存放读取到的数据
 //     * 用户退出，删除数据
 //     */
 //
-//    /**
-//     * 保存音乐源数据
-//     *
-//     * @param context
-//     */
-//    public void setMusicSource(Context context) {
-//        // 拿到资源文件中的数据
-//        String musicSourceJson = DataUtil.getJsonFromAssets(context, "DataSource.json");
-//        mRealm.beginTransaction();
-//        mRealm.createObjectFromJson(MusicSourceModel.class, musicSourceJson);
-//        mRealm.commitTransaction();
-//    }
-//
-//    /**
-//     * 删除音乐元数据
-//     */
-//    public void removeMusicSource() {
-//        mRealm.beginTransaction();
-//        mRealm.delete(MusicSourceModel.class);
-//        mRealm.delete(MusicModel.class);
-//        mRealm.delete(AlbumModel.class);
-//        mRealm.commitTransaction();
-//    }
-//
-//    /**
-//     * 返回音乐源数据
-//     */
-//    public MusicSourceModel getMusicSource() {
-//        return mRealm.where(MusicSourceModel.class).findFirst();
-//    }
-//
-//
-//    /**
-//     * 返回歌单
-//     * @param albumId
-//     * @return
-//     */
-//    public AlbumModel getAlbum(String albumId) {
-//        return mRealm.where(AlbumModel.class).equalTo("albumId", albumId).findFirst();
-//    }
-//
-//    /**
-//     * 返回音乐
-//     * @param musicId
-//     * @return
-//     */
-//    public MusicModel getMusic(String musicId) {
-//        return mRealm.where(MusicModel.class).equalTo("musicId", musicId).findFirst();
-//    }
+   /**
+    * 保存音乐资源数据
+    *
+    * @param context
+    */
+   public void setMusicSource(Context context) {
+       // 拿到资源文件中的数据
+       String musicSourceJson = DataUtil.getJsonFromAssets(context, "DataSource.json");
+       mRealm.beginTransaction();
+       // 把Json数据直接保存到数据库中 -参数说明 1. 数据模型 2. Json数据
+       mRealm.createObjectFromJson(MusicSourceModel.class, musicSourceJson);
+       mRealm.commitTransaction();
+   }
+
+   /**
+    * 删除音乐元数据
+    */
+   public void removeMusicSource() {
+       mRealm.beginTransaction();
+       mRealm.delete(MusicSourceModel.class);
+       mRealm.delete(MusicModel.class);
+       mRealm.delete(AlbumModel.class);
+       mRealm.commitTransaction();
+   }
+
+   /**
+    * 返回音乐源数据
+    */
+   public MusicSourceModel getMusicSource() {
+       return mRealm.where(MusicSourceModel.class).findFirst();
+   }
+
+
+    /**
+     * 返回歌单
+     * @param albumId
+     * @return
+     */
+    public AlbumModel getAlbum(String albumId) {
+        return mRealm.where(AlbumModel.class).equalTo("albumId", albumId).findFirst();
+    }
+
+    /**
+     * 返回音乐
+     * @param musicId
+     * @return
+     */
+    public MusicModel getMusic(String musicId) {
+        return mRealm.where(MusicModel.class).equalTo("musicId", musicId).findFirst();
+    }
 
 }
