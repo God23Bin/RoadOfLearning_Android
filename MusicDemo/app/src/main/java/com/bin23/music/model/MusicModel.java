2b6b5f22eab0b2f6fd8b83513fe8166b343f8c5c
god23bin <479017580@qq.com>
2021-06-04 20:10:37 +0800

    注释修改

--- MusicDemo/app/src/main/java/com/bin23/music/model/MusicModel.java
+++ MusicDemo/app/src/main/java/com/bin23/music/model/MusicModel.java
@@ -1,65 +1,55 @@
-package com.example.imoocmusic.Models;
+package com.bin23.music.model;
 
 import io.realm.RealmObject;
 
-/******
- * "musicId": "201",
- *           "name": "Nostalgic Piano",
- *           "poster": "http://res.lgdsunday.club/poster-1.png",
- *           "path": "http://res.lgdsunday.club/Nostalgic%20Piano.mp3",
- *           "author": "Rafael Krux"
- *
- *
- *
- ******/
 public class MusicModel extends RealmObject {
 
-    private String musicId;
-    private String name;
-    private String poster;
-    private String path;
-    private String author;
+   private String musicId;
+   private String name;
+   private String poster;
+   private String path;
+   private String author;
 
 
-    public String getMusicId() {
-        return musicId;
-    }
+   public String getMusicId() {
+       return musicId;
+   }
 
-    public void setMusicId(String musicId) {
-        this.musicId = musicId;
-    }
+   public void setMusicId(String musicId) {
+       this.musicId = musicId;
+   }
 
-    public String getName() {
-        return name;
-    }
+   public String getName() {
+       return name;
+   }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+   public void setName(String name) {
+       this.name = name;
+   }
 
-    public String getPoster() {
-        return poster;
-    }
+   public String getPoster() {
+       return poster;
+   }
 
-    public void setPoster(String poster) {
-        this.poster = poster;
-    }
+   public void setPoster(String poster) {
+       this.poster = poster;
+   }
 
-    public String getPath() {
-        return path;
-    }
+   public String getPath() {
+       return path;
+   }
 
-    public void setPath(String path) {
-        this.path = path;
-    }
+   public void setPath(String path) {
+       this.path = path;
+   }
 
-    public String getAuthor() {
-        return author;
-    }
+   public String getAuthor() {
+       return author;
+   }
 
-    public void setAuthor(String author) {
-        this.author = author;
-    }
+   public void setAuthor(String author) {
+       this.author = author;
+   }
 
 
 }
