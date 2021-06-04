26ce5d8d3b3dc924fb8551d9c244f43d6ca082fe
god23bin <479017580@qq.com>
2021-06-04 20:16:53 +0800

    将MusicModel的数据填充到View上

--- MusicDemo/app/src/main/java/com/bin23/music/adapters/MusicsListAdapter.java
+++ MusicDemo/app/src/main/java/com/bin23/music/adapters/MusicsListAdapter.java
@@ -15,6 +15,10 @@ import android.widget.TextView;
 
 import com.bin23.music.R;
 import com.bin23.music.activities.PlayMusicActivity;
+import com.bin23.music.model.MusicModel;
+import com.bumptech.glide.Glide;
+
+import java.util.List;
 
 public class MusicsListAdapter extends RecyclerView.Adapter<MusicsListAdapter.ViewHolder> {
 
@@ -24,9 +28,17 @@ public class MusicsListAdapter extends RecyclerView.Adapter<MusicsListAdapter.Vi
     private RecyclerView mRv;
     private boolean isCalculatedRvHeight;
 
+    private List<MusicModel> mDataSource;
+
     public MusicsListAdapter(Context context, RecyclerView recyclerView) {
         mContext = context;
         mRv = recyclerView;
+    }
+
+    public MusicsListAdapter(Context context, RecyclerView recyclerView, List<MusicModel> dataSource) {
+        mContext = context;
+        mRv = recyclerView;
+        mDataSource = dataSource;
     }
 
     @NonNull
@@ -43,10 +55,22 @@ public class MusicsListAdapter extends RecyclerView.Adapter<MusicsListAdapter.Vi
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         setRecyclerViewHeight();
 
+        final MusicModel musicModel = mDataSource.get(position);
+
+        Glide.with(mContext)
+                // .load("https://th.bing.com/th/id/Ra1ad377e593da8811d0a417628a1a789?rik=r9Eelwbi19NwTA&riu=http%3a%2f%2fp4.music.126.net%2fmzMD4FTkPStfqefkoiy3Eg%3d%3d%2f3222668581393466.jpg&ehk=OR3Eq4mc0ZRtJOVOGpNMqUsBxHTTpjKQ5N%2fFYLPs1c4%3d&risl=&pid=ImgRaw")
+                .load(musicModel.getPoster())
+                .into(holder.ivIcon);
+
+        holder.tvName.setText(musicModel.getName());
+        holder.tvAuthor.setText(musicModel.getAuthor());
+
         holder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(mContext, PlayMusicActivity.class);
+                Intent intent = new Intent(mContext, PlayMusicActivity.class);
+                intent.putExtra(PlayMusicActivity.MUSIC_ID, musicModel.getMusicId());
+
                 mContext.startActivity(intent);
             }
         });
@@ -54,7 +78,8 @@ public class MusicsListAdapter extends RecyclerView.Adapter<MusicsListAdapter.Vi
 
     @Override
     public int getItemCount() {
-        return 20;
+        // return 20;
+        return mDataSource.size();
     }
 
     /**
@@ -81,14 +106,14 @@ public class MusicsListAdapter extends RecyclerView.Adapter<MusicsListAdapter.Vi
 
         ImageView ivIcon;
         View itemView;
-        TextView TvName,TvAuthor;
+        TextView tvName, tvAuthor;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             this.itemView=itemView;
-            ivIcon=itemView.findViewById(R.id.iv_song_cover_icon);
-            TvName=itemView.findViewById(R.id.tv_song_name);
-            TvAuthor=itemView.findViewById(R.id.tv_song_author);
+            ivIcon = itemView.findViewById(R.id.iv_song_cover_icon);
+            tvName = itemView.findViewById(R.id.tv_song_name);
+            tvAuthor = itemView.findViewById(R.id.tv_song_author);
         }
     }
 }
